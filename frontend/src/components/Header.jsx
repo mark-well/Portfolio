0ff604b2fd782d0d5e34627d145ef572f2dd0108
mark-well <lacemerto05@gmail.com>
2025-10-29 00:01:12 +0800

    feat: add transition to navigations

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return (
         <header className="text-base min-h-16 flex items-center w-full text-[var(--text-color)]">
             <nav className="w-full">
-                <ul className="flex items-center justify-center gap-12">
+                <ul className="navigation flex items-center justify-center gap-12">
                     <a href="/home"><li>Home</li></a>
                     <a href="/about"><li>About</li></a>
                     <a href="/contact"><li>Contact</li></a>
