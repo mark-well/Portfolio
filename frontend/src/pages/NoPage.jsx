ad921e508ba8a964fd39400d330d2f870cf83b3c
mark-well <lacemerto05@gmail.com>
2025-10-28 23:46:50 +0800

    feat: add 404 page

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,5 +1,6 @@
 
 import { Helmet } from "react-helmet";
+import Header from '../components/Header'
 
 function NoPage() {
     return(
@@ -7,6 +8,19 @@ function NoPage() {
             <Helmet>
                 <title>404</title>
             </Helmet>
+            <div className="no-page-container h-dvh w-full">
+                <Header />
+                <h1 className="font-bold text-8xl text-center mt-28">404</h1>
+                <section className='info-section mt-14 about-section bg-[var(--primary-color)] font-montserrat'>
+                    <h2 className='font-bold text-4xl'>Oops!</h2>
+                    <div className='flex flex-col gap-y-8 text-justify'>
+                        <p className='max-w-3xl'>
+                            Sorry, the page you're looking for doesn't seem to exist in this universe.
+                        </p>
+                    </div>
+                </section>
+
+            </div>
         </>
     );
 }
